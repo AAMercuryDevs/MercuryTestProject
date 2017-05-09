7f2c4ccbac8303c0844652da379cbf64d290f575
841386 <suhas.bahekar@aa.com>
2017-05-09 14:06:31 -0500

    changes Sysout and added a for loop

--- GitTest.java
+++ GitTest.java
@@ -4,7 +4,9 @@ public class GitTest {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("This is a test project");
+		for(int i =0; i <5; i++){
+			System.out.println("This is a test project for Loop for suhas : "+ i);
+		}
+		System.out.println("This is a test project for Loop for Suhas");
 	}
-
 }
