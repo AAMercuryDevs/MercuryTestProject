b780ce929a46454f49ae337f322bbdf011125ebe
789402 <789402@QJ08051042.corpaa.aa.com>
2017-05-09 14:00:15 -0500

    mercury story one

--- GitTest.java
+++ GitTest.java
@@ -1,10 +1,17 @@
 package test;
 
 public class GitTest {
+	
+	public int mercury=1;
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("This is a test project");
+		if(mercury==1){
+			System.out.println("This is mercury");
+		}else{
+			System.out.println("This is not mercury");
+		}
 	}
 
 }
